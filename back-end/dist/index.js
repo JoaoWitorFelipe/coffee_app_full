0ef4564d9dd6e7fdccc51d4e896d4cf1c78b6c37
JoaoWitorFelipe <joaowitorfelipe2@gmail.com>
2020-12-09 06:30:19 -0300

    remove dist folder

--- back-end/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-(async () => {
-    const server = await server_1.createServer();
-    server.listen(3000).then((url) => {
-        console.log(`🚀  Server ready at ${url}`);
-    });
-})();
